1a9f8b730d09daa0abfe178b100fa658db16f44c
0cked <jtk1014@gmail.com>
2026-01-20 15:37:55 -0500

    fix2

--- lib/louis.ts
+++ lib/louis.ts
@@ -14,9 +14,9 @@ export type LouisEngine = {
 };
 
 const ASSET_BASE = "liblouis";
-const CAPI_PATH = `/${ASSET_BASE}/build-no-tables-utf16.js`;
-const EASY_API_PATH = `/${ASSET_BASE}/easy-api.js`;
-const TABLES_PATH = `/${ASSET_BASE}/tables/`;
+const CAPI_PATH = `${ASSET_BASE}/build-no-tables-utf16.js`;
+const EASY_API_PATH = `${ASSET_BASE}/easy-api.js`;
+const TABLES_PATH = `${ASSET_BASE}/`;
 
 let cachedEngine: Promise<LouisEngine> | null = null;
 
